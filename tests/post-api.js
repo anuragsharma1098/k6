ff61628afc2ba909a26b28351ad05cb24256782e
anuragsharma1098 <anuragsharma1098@gmail.com>
2026-03-31 15:22:08 +0530

    post api

--- tests/post-api.js
+++ tests/post-api.js
@@ -7,14 +7,26 @@ export const options = {
 };
 
 const url = "https://reqres.in/api/users";
-const payload = {
-  "name": "John Doe",
-  "job": "Software Engineer",
-};
+// const payload = {
+//   "name": "John Doe",
+//   "job": "Software Engineer",
+// };
+
+// export default function () {
+//   const response = http.post(url, payload);
+//   console.log("Payload sent:", payload);
+//   console.log("Response body:", response.body);
+//   check(response, {
+//     "is status 201": (response) => response.status === 201,
+//     "response has id": (response) => response.body.includes("id"),
+//   });
+// }
+
+const data = JSON.parse(open("./payload.json"));
 
 export default function () {
-  const response = http.post(url, payload);
-  console.log("Payload sent:", payload);
+  const response = http.post(url, data);
+  console.log("Payload sent:", data);
   console.log("Response body:", response.body);
   check(response, {
     "is status 201": (response) => response.status === 201,

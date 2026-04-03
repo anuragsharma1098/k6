56c367d28491c45ae6587a45024a86c62df798a7
anuragsharma1098 <anuragsharma1098@gmail.com>
2026-04-03 16:17:38 +0530

    taking ss

--- tests/browserTest.js
+++ tests/browserTest.js
@@ -19,5 +19,11 @@ export const options = {
 export default async function () {
   const page = await browser.newPage();
   await page.goto("https://www.google.com/");
+  // await page.screenshot({ path: `../screenshots/google-${__VU}.png` });
+  const timestamp = new Date().toISOString().replace(/[:.]/g, "-");
+  await page.screenshot({
+    fullPage: true,
+    path: `../screenshots/google-${timestamp}-${__VU}.png`,
+  });
   await page.close();
 }
